37d8d9eb8504f42ef1f0ffcf820b5cb8372fc72b
SornDev <sorndev.teach@gmail.com>
2022-11-29 20:05:59 +0700

    ຄອມມິດຄັ້ງທຳອິດ first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1> Hello Laravel + Vue V1 </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Minipos5App',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+    computed(){
+
+    },
+    whatch:{
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
